fd8d0b3c273a947293b0e88db4f4c801af154510
oleh420 <o732264366@gmail.com>
2023-01-24 08:24:33 +0200

    commit from bubuntu

--- src/jclevel6/Solution.java
+++ src/jclevel6/Solution.java
@@ -1,72 +1,64 @@
 package jclevel6;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
-1. Change the Violin class to make it a task for the thread. Use the MusicalInstrument interface
-2. Implement the required method in the Violin thread. The implementation should be as follows:
-2.1. Determine the time when you start playing - the startPlaying() method.
-2.2. Wait 1 second - the sleepNSeconds(int n) method, where n is the number of seconds.
-2.3. Determine the time when you stop playing - the stopPlaying() method.
-2.4. Display how long you played the instrument. Use the methods in Items 2.1 and 2.3.
 
-Example: Played for 1002 ms
+1. Figure out what the program does.
+2. We need to be sure that Chump talks the most during the political debate.
+3. Think about what method you can call on the chump object to make Chump
+talk until all the available time has been taken.
 
 Requirements:
-•	The Violin class should not inherit any additional class.
-•	The Violin class should implement the MusicalInstrument interface.
-•	The Violin class's run method should call the startPlaying method.
-•	The Violin class's run method should call the sleepNSeconds method with an argument of 1 second.
-•	The Violin class's run method should call the stopPlaying method.
-•	The Violin class's run method should display how long the instrument was played (in milliseconds). Use the format given in the example.
+•	Call the join method on the necessary object.
+•	The Politician class's toString method should display how many times the politician spoke, for example: "Chump spoke 35 times".
+•	The program should create 3 Politician objects.
+•	Don't change the methods that are responsible for screen output.
+•	The program's output should indicate that Chump spoke the most during the political debate.
 
 */
 
 public class Solution {
-    public static int delay = 1000;
+    public static int totalSpeechCount = 200;
+    public static int utterancesPerSpeech = 1_000_000;
 
-    public static void main(String[] args) {
-        Thread violin = new Thread(new Violin("Player"));
-        violin.start();
-    }
+    public static void main(String[] args) throws InterruptedException {
+        Politician chump = new Politician("Chump");
+        chump.join();
+        Politician dustbin = new Politician("Dustbin");
+        Politician schooner = new Politician("Schooner");
 
-    public static void sleepNSeconds(int n) {
-        try {
-            Thread.sleep(n * delay);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public interface MusicalInstrument extends Runnable {
-        Date startPlaying();
-
-        Date stopPlaying();
-    }
-
-    public static class Violin implements MusicalInstrument {
-        private String owner;
-
-        public Violin(String owner) {
-            this.owner = owner;
+        while (chump.getSpeechCount() + dustbin.getSpeechCount() + schooner.getSpeechCount() < totalSpeechCount) {
         }
 
-        public Date startPlaying() {
-            System.out.println(this.owner + " is starting to play");
-            return new Date();
+
+        System.out.println(chump);
+        System.out.println(dustbin);
+        System.out.println(schooner);
+    }
+
+    public static class Politician extends Thread {
+        private volatile int utteranceCount;
+
+        public Politician(String name) {
+            super(name);
+            start();
         }
 
-        public Date stopPlaying() {
-            System.out.println(this.owner + " is stopping playing");
-            return new Date();
+        public void run() {
+            while (utteranceCount < totalSpeechCount * utterancesPerSpeech) {
+                utteranceCount++;
+            }
+        }
+
+        public int getSpeechCount() {
+            return utteranceCount / utterancesPerSpeech;
         }
 
         @Override
-        public void run() {
-            Date start = startPlaying();
-            sleepNSeconds(1);
-            Date stop = stopPlaying();
-            System.out.printf("Played for %d ms", (stop.getTime() - start.getTime()));
+        public String toString() {
+            return String.format("%s spoke %d times", getName(), getSpeechCount());
         }
     }
 }
