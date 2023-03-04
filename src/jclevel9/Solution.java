19780201ec1736c19b2748e9c46f85320b149053
oleh420 <o732264366@gmail.com>
2023-03-04 07:49:47 +0200

    haha

--- src/jclevel9/Solution.java
+++ src/jclevel9/Solution.java
@@ -1,41 +1,79 @@
 package jclevel9;
 
-/*Replace the main method's System.out object with your own reader wrapper similar
- to the one shown in the lesson.
-Your reader wrapper should convert all text to uppercase letters.
-Call testString's existing printSomething() method.
-Restore the System.out variable back to the original stream.
-Display the modified string.
+/*Read 2 file names from the console: file1 and file2.
+Both files contain text, but file2 is an updated version of file1. Some of the lines are still the same.
+You need to create a merged version of the lines by writing them to the lines list.
+The ADDED and REMOVED labels can't be used consecutively—they must always separated by the SAME label.
+The example includes empty lines for clarity.
+Neither the original file nor the updated file have empty lines!
+
+Example 1:
+
+In the example, empty lines mean that the line is not in the specified file.
 
 Requirements:
-•	The Solution class must contain a TestString class.
-•	The Solution class must have a public static TestString field called
-testString that is initialized immediately.
-•	The TestString class must have a public void printSomething() method.
-•	The TestString class's printSomething() method must display "This is text for testing".
-•	The Solution class's main(String[] args) method must create a PrintStream object
-(use the PrintStream constructor with a ByteArrayOutputStream parameter).
-•	The Solution class's main(String[] args) method should replace and then restore
-the console output stream (System.out).
-•	The Solution class's main(String[] args) method must call testString's printSomething() method.
-•	The Solution class's main(String[] args) method must modify the line printed by
-the printSomething() method, as per the task conditions, and output it to the console.*/
+•	The Solution class must contain a LineItem class.
+•	The Solution class must have an enum called Type.
+•	The Solution class must have a public static List<LineItem> field called lines that is initialized immediately.
+•	In the main(String[] args) method, the program must read file names from the console (use BufferedReader).
+•	In the main(String[] args) method, the BufferedReader used for reading input from the console must be
+closed after use.
+•	The program must read the contents of the first and second file (use FileReader).
+•	The file input streams (FileReader) must be closed.
+•	The lines list should contain the merged version of the lines from the files.
+Each line should start with the label ADDED, REMOVED, or SAME, depending on the action taken.
+ */
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/*
+Tracking changes
+
+*/
 
 public class Solution {
-    public static TestString testString = new TestString();
+    public static List<LineItem> lines = new ArrayList<>();
 
     public static void main(String[] args) {
+        try (BufferedReader bufferedReader =
+                     new BufferedReader(new InputStreamReader(System.in));
+             FileReader fileReader1 =
+                     new FileReader(bufferedReader.readLine());
+             FileReader fileReader2 =
+                     new FileReader(bufferedReader.readLine());
+             BufferedReader reader1 = new BufferedReader(fileReader1);
+             BufferedReader reader2 = new BufferedReader(fileReader2)) {
+
+            Stream.
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static class TestString {
-        public void printSomething() {
-            System.out.println("This is text for testing");
+    public static enum Type {
+        ADDED,        // New line added
+        REMOVED,      // Line deleted
+        SAME          // No change
+    }
+
+    public static class LineItem {
+        public Type type;
+        public String line;
+
+        public LineItem(Type type, String line) {
+            this.type = type;
+            this.line = line;
         }
     }
 }
+
 
 
 
